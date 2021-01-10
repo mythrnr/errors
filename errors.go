bf38e44e34f4ce0e33fa4bf9ff7fa31904a82f4d
mythrnr <mythrnr@gmail.com>
2021-01-10 23:55:27 +0900

    Some updates
    - [add] settings for editor.
    - [modify] write README.md
    - [modify] use self package funcs instead using standard errors directly.
    - [modify] use self `New()` in tests instead `fmt.Errorf()` .

--- errors.go
+++ errors.go
@@ -1,39 +1,73 @@
+// Package errors はエラーをラップする機能を提供する.
+//
+// Go言語の標準パッケージである `errors` の `Is` , `As` , `Unwrap` を利用することで,
+// 内包するエラーを取り出して利用することができる.
+//
+// - ラップするときに `fmt.Errorf("%w", err)` のように文字列のメッセージではなく
+// `error` オブジェクトをそのまま渡す.
+//
+// - `error` オブジェクトを内包させるので, 定義済みエラーをネストさせて
+// `errors.Is` で判定ができる.
 package errors
 
-import (
-	stderr "errors"
-)
-
-type wrappedError struct {
-	err   error
+type wrappingErr struct {
+	main  error
 	cause error
 }
 
-var _ error = (*wrappedError)(nil)
+var (
+	_ error                             = (*wrappingErr)(nil)
+	_ interface{ As(interface{}) bool } = (*wrappingErr)(nil)
+	_ interface{ Is(error) bool }       = (*wrappingErr)(nil)
+	_ interface{ Unwrap() error }       = (*wrappingErr)(nil)
+)
 
+// Wrap returns the error object includes `err` and `cause` object.
+// Returns `nil` if `err` is `nil`.
+//
+// Wrap は `err` と `cause` を含んだエラーオブジェクトを返す.
+// `err` が `nil` の場合は `nil` が返される.
 func Wrap(err, cause error) error {
 	if err == nil {
 		return nil
 	}
 
-	return &wrappedError{
-		err:   err,
+	return &wrappingErr{
+		main:  err,
 		cause: cause,
 	}
 }
 
-func (e *wrappedError) As(target interface{}) bool {
-	return stderr.As(e.err, target) || stderr.As(e.cause, target)
+// As searches for value that can be assigned to `target`
+// with the priority of `e.main`, `e.cause`,
+// and returns `true` if the value can be assigned.
+//
+// As は `target` に代入可能な値を `e.main` , `e.cause` の優先順位で探し,
+// 代入できた場合は `true` を返す.
+func (e *wrappingErr) As(target interface{}) bool {
+	return As(e.main, target) || As(e.cause, target)
 }
 
-func (e *wrappedError) Error() string {
-	return e.err.Error()
+// Error calls `Error` in `e.main` and returns message.
+//
+// Error は `e.main` の `Error` を呼び出してメッセージを返す.
+func (e *wrappingErr) Error() string {
+	return e.main.Error()
 }
 
-func (e *wrappedError) Is(err error) bool {
-	return stderr.Is(e.err, err) || stderr.Is(e.cause, err)
+// Is searches for an error object matching `err`
+// with the priority of `e.main`, `e.cause`,
+// and returns `true` if it matches.
+//
+// Is は `err` に一致するエラーオブジェクトを
+// `e.main` , `e.cause` の優先順位で探し, 一致した場合は `true` を返す.
+func (e *wrappingErr) Is(err error) bool {
+	return Is(e.main, err) || Is(e.cause, err)
 }
 
-func (e *wrappedError) Unwrap() error {
+// Unwrap returns the inner `e.cause`.
+//
+// Unwrap は内側の `e.cause` を返す.
+func (e *wrappingErr) Unwrap() error {
 	return e.cause
 }
--- errors_test.go
+++ errors_test.go
@@ -1,7 +1,6 @@
 package errors_test
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/mythrnr/errors"
@@ -14,13 +13,13 @@ func (e *myError) Error() string { return e.msg }
 
 func Test_Wrap(t *testing.T) {
 	assert.Nil(t, errors.Wrap(nil, nil))
-	assert.NotNil(t, errors.Wrap(fmt.Errorf("error"), nil))
-	assert.NotNil(t, errors.Wrap(fmt.Errorf("error"), fmt.Errorf("cause")))
+	assert.NotNil(t, errors.Wrap(errors.New("main"), nil))
+	assert.NotNil(t, errors.Wrap(errors.New("main"), errors.New("cause")))
 }
 
-func Test_wrappedError_As(t *testing.T) {
+func Test_wrappingErr_As(t *testing.T) {
 	{
-		err := errors.Wrap(fmt.Errorf("error"), nil)
+		err := errors.Wrap(errors.New("main"), nil)
 		me := &myError{}
 
 		assert.False(t, errors.As(err, &me))
@@ -28,64 +27,73 @@ func Test_wrappedError_As(t *testing.T) {
 	}
 
 	{
-		err := errors.Wrap(fmt.Errorf("error"), &myError{msg: "cause"})
+		err := errors.Wrap(errors.New("main"), &myError{msg: "cause"})
 		me := &myError{}
 
 		assert.True(t, errors.As(err, &me))
 		assert.Equal(t, "cause", me.Error())
 	}
+
+	{
+		err := errors.Wrap(&myError{msg: "main"}, &myError{msg: "cause"})
+		me := &myError{}
+
+		assert.True(t, errors.As(err, &me))
+		assert.Equal(t, "main", me.Error())
+	}
 }
 
-func Test_wrappedError_Error(t *testing.T) {
+func Test_wrappingErr_Error(t *testing.T) {
 	assert.Equal(t,
-		"error",
-		errors.Wrap(fmt.Errorf("error"), nil).Error(),
+		"main",
+		errors.Wrap(errors.New("main"), nil).Error(),
 	)
 
 	assert.Equal(t,
-		"error",
-		errors.Wrap(fmt.Errorf("error"), fmt.Errorf("cause")).Error(),
+		"main",
+		errors.Wrap(errors.New("main"), errors.New("cause")).Error(),
 	)
 }
 
-func Test_wrappedError_Is(t *testing.T) {
-	ErrTest := fmt.Errorf("defined error")
+func Test_wrappingErr_Is(t *testing.T) {
+	ErrTest := errors.New("defined error")
 
 	{
-		err := errors.Wrap(fmt.Errorf("error"), nil)
+		err := errors.Wrap(errors.New("main"), nil)
 		assert.False(t, errors.Is(err, ErrTest))
 	}
 
 	{
-		err := errors.Wrap(fmt.Errorf("error"), fmt.Errorf("cause"))
+		err := errors.Wrap(errors.New("main"), errors.New("cause"))
 		assert.False(t, errors.Is(err, ErrTest))
 	}
 
 	{
-		err := errors.Wrap(fmt.Errorf("error"), fmt.Errorf("defined error"))
+		err := errors.Wrap(errors.New("main"), errors.New("defined error"))
 		assert.False(t, errors.Is(err, ErrTest))
 	}
 
 	{
-		err := errors.Wrap(ErrTest, fmt.Errorf("error"))
+		err := errors.Wrap(ErrTest, errors.New("cause"))
 		assert.True(t, errors.Is(err, ErrTest))
 	}
 
 	{
-		err := errors.Wrap(fmt.Errorf("error"), ErrTest)
+		err := errors.Wrap(errors.New("main"), ErrTest)
 		assert.True(t, errors.Is(err, ErrTest))
 	}
 }
 
-func Test_wrappedError_Unwrap(t *testing.T) {
+func Test_wrappingErr_Unwrap(t *testing.T) {
 	{
-		err := errors.Wrap(fmt.Errorf("error"), nil)
+		err := errors.Wrap(errors.New("main"), nil)
 		assert.Nil(t, errors.Unwrap(err))
 	}
 
 	{
-		err := errors.Wrap(fmt.Errorf("error"), fmt.Errorf("cause"))
+		err := errors.Wrap(errors.New("main"), errors.New("cause"))
 		ue := errors.Unwrap(err)
+
 		assert.NotNil(t, ue)
 		assert.Equal(t, "cause", ue.Error())
 	}
